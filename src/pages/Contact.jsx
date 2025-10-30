2ee7cdf76154429efbed46a4238dd96e27be8b35
TetuiDev <pablo.canales.01@gmail.com>
2025-10-30 14:15:04 -0600

    added functions to send mail using EmailJs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,105 @@
-export default function Contact() {
-  return (
-    <section className="p-8 md:p-16 text-center">
-      <h2 className="text-3xl font-semibold mb-4 text-[#F49C9C]">Pedidos</h2>
-      <p className="text-[#706a64]">A partir de 50 unidades</p>
-      <p className="text-[#706a64]">Fechas de entrega a trata sobre pedidos</p>
-      <p className="text-[#706a64] mb-6">Se requiere % pago inicial en base a la cantidad </p>
+import { useRef,useState } from 'react';
+import emailjs from '@emailjs/browser'
 
+export default function Contact() {
+    const [form, setForm] = useState({ user_name: "", user_email: "", user_phone: "", user_message: "" });
+    const [errors, setErrors] = useState({});
+    const [sended,setSended] = useState(false)
+    const formSend = useRef();
+
+    const validate = () => {
+        const newErrors = {};
+        if (!form.user_name.trim()) newErrors.name = "Nombre es requerido";        
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(form.user_email)) newErrors.email = "Correo valido es requerido";
+        const phonePattern = /^[0-9]{7,15}$/;
+        if (!phonePattern.test(form.user_phone)) newErrors.phone = "Teléfono es requerido (7–15 digitos)";
+        if (form.user_message.trim().length < 5)
+        newErrors.message = "Mensaje es requerido";
+        return newErrors;
+    };
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const sendEmail = (e) => {
+        e.preventDefault();        
+        const validationErrors = validate();
+        if(Object.keys(validationErrors).length > 0){
+            setErrors(validationErrors);
+            return;
+        }
+        setSended(true)
+        setErrors({});
+        emailjs
+        .sendForm(
+            'service_s2gwahd',
+            'template_6oya10o',
+            formSend.current, 
+            { publicKey: 'xj9JDvEefzE5kHCgo',}
+        )
+        .then(
+            () => {
+                console.log('SUCCESS!');
+            },
+            (error) => {
+                console.log('FAILED...', error.text);
+            },
+        );
+    };
+
+    return (
+        <section className="p-8 md:p-16 text-center">
+            <h2 className="text-3xl font-semibold mb-4 text-[#F49C9C]">Pedidos</h2>
+            <p className="text-[#706a64]">A partir de 50 unidades</p>
+            <p className="text-[#706a64]">Fechas de entrega a trata sobre pedidos</p>
+            <p className="text-[#706a64] mb-6">Se requiere % pago inicial en base a la cantidad </p>
       
-      
-      <form className="max-w-md mx-auto flex flex-col gap-4">
-        <input type="text" placeholder="Nombre" className="p-2 border rounded-md text-[#706a64]" />
-        <input type="email" placeholder="Correo" className="p-2 border rounded-md text-[#706a64]" />
-        <input type="number" placeholder="Teléfono" className="p-2 border rounded-md text-[#706a64]" />
-        <textarea placeholder="Mensaje" rows="4" className="p-2 border rounded-md text-[#706a64]"></textarea>
-        <button className="bg-[#b4825e] text-white py-2 rounded-md hover:bg-[#8e6449]">
-          Enviar
-        </button>
-      </form>
-    </section>
-  );
+            <form ref={formSend} onSubmit={sendEmail} className="max-w-md mx-auto flex flex-col gap-4">
+                <input 
+                type="text" 
+                name="user_name" 
+                placeholder="Nombre" 
+                onChange={handleChange}
+                className="p-2 border rounded-md text-[#706a64]" />
+                {errors.name && <p className="text-red-500 text-sm m-0">{errors.name}</p>}
+
+                <input 
+                type="email" 
+                name="user_email" 
+                placeholder="Correo" 
+                onChange={handleChange}
+                className="p-2 border rounded-md text-[#706a64]" />
+                {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
+                
+                <input 
+                type="number" 
+                name="user_phone" 
+                placeholder="Teléfono" 
+                onChange={handleChange}
+                className="p-2 border rounded-md text-[#706a64]" />
+                {errors.phone && <p className="text-red-500 text-sm">{errors.phone}</p>}
+
+                <textarea 
+                placeholder="Mensaje" 
+                name="user_message" 
+                rows="4" 
+                onChange={handleChange}
+                className="p-2 border rounded-md text-[#706a64]"></textarea>
+                {errors.message && <p className="text-red-500 text-sm">{errors.message}</p>}
+
+                {sended?
+                <div className="text-[white] bg-[#F49C9C] h-[60px]">
+                    <p className="text-lg pt-4 font-medium">Pedido enviado, pronto nos podremos en contacto!!!.</p>
+                </div>
+                :
+                <button className="bg-[#b4825e] text-white py-2 rounded-md hover:bg-[#8e6449]">
+                Enviar
+                </button>
+                }
+                
+            </form>
+        </section>
+    );
 }
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -21,8 +21,7 @@ export default function Test (){
                 </div>
                 <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">      Message    </button>
             </div>
-        </div>
-        
+        </div>        
         </>
     )
 }
